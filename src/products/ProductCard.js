78159e4e9a9fbea2b7c1704875aa8ebd54b235cf
RuwanSudheera <sudheerwim@gmail.com>
2023-06-18 00:11:32 +0530

    basic css changes added to match with given template

--- src/products/ProductCard.js
+++ src/products/ProductCard.js
@@ -4,10 +4,11 @@ const ProductCard = ({ product }) => {
     console.log('product', product);
   return (
     <div className="product-card">
+      <span>Tap to view details</span>
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>{product.description}</p>
-      <span>{product.price}</span>
+      <span className="product-card-price">{product.price}</span>
     </div>
   );
 };
