abdf1edf9c8a013496bb264f5e5e18d5a333acc5
RuwanSudheera <sudheerwim@gmail.com>
2023-06-17 23:41:46 +0530

    Initial commit of soul_studio front portal

---
+++ src/ProductList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductCard from './ProductCard';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:3006/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
