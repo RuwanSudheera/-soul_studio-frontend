116eb054efdb1f5ba4f6e8b9334f96d2979ac635
RuwanSudheera <sudheerwim@gmail.com>
2023-06-20 00:09:39 +0530

    Update css files for matching with requested template

--- src/products/ProductCard.js
+++ src/products/ProductCard.js
@@ -4,11 +4,17 @@ const ProductCard = ({ product }) => {
     console.log('product', product);
   return (
     <div className="product-card">
-      <span>Tap to view details</span>
-      <img src={product.image} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <span className="product-card-price">{product.price}</span>
+      <div className="product-card-body">
+        <span>Tap to view details</span>
+        <img src={product.image} alt={product.name} />
+        <h3>{product.name}</h3>
+        <p>{product.description}</p>
+        <span className="product-card-price">${product.price}</span>
+      </div>
+      <div className="button-container">
+        <button className="rounded-button button-1"> - <span className="button-line">1</span><span className="button-line">Nos</span> + </button>
+        <button className="rounded-button button-2"><i className="fas fa-shopping-cart"></i>Add to cart</button>
+      </div>
     </div>
   );
 };
